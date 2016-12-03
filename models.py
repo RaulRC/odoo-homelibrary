4562bd5ba670c17b3c98de1bb2db6e74ebddf48e
RRC <raul.elfriky@gmail.com>
2016-12-03 20:28:27 +0100

    [IMP] minimal functionality

--- models.py
+++ models.py
@@ -6,8 +6,10 @@ class Book(models.Model):
     _name = 'homelibrary.book'
     title = fields.Char(required=True, string='Título')
     rack = fields.Integer(required=True, string='Estantería')
+    author_id = fields.Many2one('homelibrary.author', string='Autor')
 
 class Author(models.Model):
     _name = 'homelibrary.author'
     name = fields.Char(required=True, string='Nombre')
+    book_ids = fields.One2many('homelibrary.book', 'author_id', string='Libros del autor')
      
