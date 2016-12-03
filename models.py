339f06b41c4fdd33fb588aef4a94ac4aa126d16b
RRC <raul.elfriky@gmail.com>
2016-12-03 20:47:42 +0100

    [IMP] model names and computed names

--- models.py
+++ models.py
@@ -3,11 +3,23 @@
 from openerp import models, fields, api
 
 
+class Homelibrary(models.Model):
+    _name = 'homelibrary.homelibrary'
+    owner = fields.Many2one('res.users', string='Propietario')
+    name = fields.Char(required=True, string='Nombre')
+
 class Book(models.Model):
     _name = 'homelibrary.book'
+    name = fields.Char(compute='_compute_name')
     title = fields.Char(required=True, string='Título')
-    rack = fields.Integer(required=True, string='Estantería')
+    rack = fields.Integer(required=False, string='Estantería')
     author_id = fields.Many2one('homelibrary.author', string='Autor')
+    library_id = fields.Many2one('homelibrar.homelibrary', string='Biblioteca')
+
+    @api.multi
+    def _compute_name(self):
+        for record in self: 
+            record.name = "%s (%s)" % (record.title, record.author_id.name)
 
 class Author(models.Model):
     _name = 'homelibrary.author'
